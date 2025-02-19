582943499eb98ba95430551714e63fa891e42cd6
Wesley Aldrich <169461537+wesleyaldrich@users.noreply.github.com>
2025-02-19 23:49:08 +0700

    Initial commit for flask-genhive.

---
+++ api.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import librosa
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import tensorflow as tf
+from tensorflow.image import resize # type: ignore
+
+MODEL_FOLDER = os.path.join(os.getcwd(), 'model')
+
+def genhive(file_path):
+    # get the model
+    model_path = os.path.join(MODEL_FOLDER, "GenHive_model.h5")
+    model = tf.keras.models.load_model(model_path)
+
+    # define genres
+    classes = ['Alternative/Indie',
+               'EDM', 'J-Pop', 'K-Pop',
+               'Pop', 'RNB', 'Rock',
+               'Trap/Hip-Hop']
+    
+    # preprocess the target file
+    X_test = load_and_preprocess_data(file_path)
+
+    # predict the genre
+    y_pred = model.predict(X_test)
+    predicted_categories = np.argmax(y_pred,axis=1)
+    unique_elements, counts = np.unique(predicted_categories, return_counts=True)
+    max_count = np.max(counts)
+    max_elements = unique_elements[counts == max_count]
+
+    # return the predicted genre
+    return classes[max_elements[0]]
+
+def load_and_preprocess_data(file_path, target_shape=(150, 150)):
+    data = []
+    audio_data, sample_rate = librosa.load(file_path, sr=None)
+    # Perform preprocessing (e.g., convert to Mel spectrogram and resize)
+    # Define the duration of each chunk and overlap
+    chunk_duration = 4  # seconds
+    overlap_duration = 2  # seconds
+
+    # Convert durations to samples
+    chunk_samples = chunk_duration * sample_rate
+    overlap_samples = overlap_duration * sample_rate
+
+    # Calculate the number of chunks
+    num_chunks = int(np.ceil((len(audio_data) - chunk_samples)
+                             / (chunk_samples - overlap_samples))) + 1
+
+    # Iterate over each chunk
+    for i in range(num_chunks):
+        start = i * (chunk_samples - overlap_samples)
+        end = start + chunk_samples
+
+        chunk = audio_data[start:end]
+
+        mel_spectrogram = librosa.feature.melspectrogram(y=chunk, sr=sample_rate)
+
+        mel_spectrogram = resize(np.expand_dims(mel_spectrogram, axis=-1), target_shape)
+        data.append(mel_spectrogram)
+
+    return np.array(data)
---
+++ predict.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from api import genhive
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    if file:
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+        print(f"Saving file to: {file_path}")
+        
+        result = {"genre": genhive(file_path)}
+        
+        return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
